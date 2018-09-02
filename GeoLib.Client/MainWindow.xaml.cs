67dfb131d7bada9b25e58679564da205f16ee635
Frank J. Reashore <reashore@telus.net>
2018-09-02 10:38:11 -0700

    Synced config files

--- GeoLib.Client/MainWindow.xaml.cs
+++ GeoLib.Client/MainWindow.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.ServiceModel;
-using System.ServiceModel.Channels;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using GeoLib.Contracts;
 using GeoLib.Proxy;
 
@@ -21,13 +20,18 @@ namespace GeoLib.Client
             Title = $"GeoLib WCF Client App: Thread ID = {managedThreadId}, Process ID = {currentProcessId}";
         }
 
+        private bool _isNetTcpBindingChecked = true;
+
         private void GetZipCodeInfoButton_Click(object sender, RoutedEventArgs e)
         {
+            // todo: fails to clear labels
+            //CityOutputLabel.Content = "";
+            //StateOutputLabel.Content = "";
+
             // ReSharper disable once InvertIf
             if (ZipCodeTextBox.Text != "")
             {
-                const string endpoint = "netTcpEndpoint";
-                GeoClient geoClient = new GeoClient(endpoint);
+                GeoClient geoClient = GetGeoClient(_isNetTcpBindingChecked);
                 ZipCodeData zipCodeData = geoClient.GetZipCodeInfo(ZipCodeTextBox.Text);
 
                 if (zipCodeData != null)
@@ -42,12 +46,18 @@ namespace GeoLib.Client
 
         private void GetZipCodesButton_Click(object sender, RoutedEventArgs e)
         {
+            // todo: Fails to clear ListBox
+            //ZipCodesListBox.ItemsSource = "";
+
             // ReSharper disable once InvertIf
             if (StateTextBox.Text != "")
             {
-                EndpointAddress endpointAddress = new EndpointAddress("net.tcp://localhost:8009/GeoService");
-                Binding binding = new NetTcpBinding();
-                GeoClient geoClient = new GeoClient(binding, endpointAddress);
+                // Alternate constructor
+                //EndpointAddress endpointAddress = new EndpointAddress("net.tcp://localhost:8009/GeoService");
+                //Binding binding = new NetTcpBinding();
+                //GeoClient geoClient = new GeoClient(binding, endpointAddress);
+
+                GeoClient geoClient = GetGeoClient(_isNetTcpBindingChecked);
                 IEnumerable<ZipCodeData> zipCodeDataEnumerable = geoClient.GetZipCodes(StateTextBox.Text);
 
                 if (zipCodeDataEnumerable != null)
@@ -57,6 +67,43 @@ namespace GeoLib.Client
                 
                 geoClient.Close();
             }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            // todo: clean up this code
+            if (e.OriginalSource is RadioButton radioButton)
+            {
+                if ((string)radioButton.Content == "netTcpBinding")
+                {
+                    _isNetTcpBindingChecked = true;
+                }
+                else
+                {
+                    _isNetTcpBindingChecked = false;
+                }
+
+            }
+        }
+
+        private static GeoClient GetGeoClient(bool isNetTcpBinding)
+        {
+            GeoClient geoClient;
+
+            if (isNetTcpBinding)
+            {
+                const string netTcpEndpoint = "netTcpEndpoint";
+                geoClient = new GeoClient(netTcpEndpoint);
+                //Debug.WriteLine("binding = netTcpEndpoint");
+            }
+            else
+            {
+                const string httpEndpoint = "basicHttpEndpoint";
+                geoClient = new GeoClient(httpEndpoint);
+                //Debug.WriteLine("binding = httpEndpoint");
+            }
+
+            return geoClient;
         }
     }
 }
